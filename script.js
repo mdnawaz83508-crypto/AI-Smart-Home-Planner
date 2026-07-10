18397848c3e518332715d4732e8847f2c1e4362e
Md Nawaz <mdnawaz83508@gmail.com>
2026-07-10 16:23:03 +0530

    Update searchAnything function to show alert
    Removed search functionality and added alert for button.

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
+alert("JavaScript connected!");
+
 function searchAnything(){
-
-let question = document.getElementById("question").value;
-
-if(question==""){
-document.getElementById("result").innerHTML="Enter something";
-return;
-}
-
-
-document.getElementById("result").innerHTML =
-"Searching for: " + question;
-
-
-setTimeout(function(){
-
-let choice = confirm(
-"Open YouTube for videos? Press OK. Press Cancel for Images."
-);
-
-
-if(choice){
-
-window.open(
-"https://www.youtube.com/results?search_query=" +
-encodeURIComponent(question)
-);
-
-}
-else{
-
-window.open(
-"https://www.google.com/search?tbm=isch&q=" +
-encodeURIComponent(question)
-);
-
-}
-
-},500);
-
-
+    alert("Button is working!");
 }
