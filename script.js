ffe76a423b0d977087d50d0f957fc4c7bf1f1852
Md Nawaz <mdnawaz83508@gmail.com>
2026-07-10 16:38:15 +0530

    Add search feature with redirect options
    Implement search functionality with user choice for YouTube or Google Images.

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-alert("NEW SCRIPT LOADED");
+document.getElementById("searchBtn").onclick = function(){
 
-function searchAnything(){
+let question = document.getElementById("question").value;
 
-alert("SEARCH BUTTON WORKING");
+
+if(question.trim() === ""){
+document.getElementById("result").innerHTML =
+"Please enter something";
+return;
+}
+
+
+document.getElementById("result").innerHTML =
+"Searching: " + question;
+
+
+// Open YouTube or Google Images
+
+let choice = confirm(
+"Press OK for YouTube videos\nPress Cancel for Google Images"
+);
+
+
+if(choice){
+
+window.location.href =
+"https://www.youtube.com/results?search_query="
++ encodeURIComponent(question);
 
 }
+else{
+
+window.location.href =
+"https://www.google.com/search?tbm=isch&q="
++ encodeURIComponent(question);
+
+}
+
+
+};
